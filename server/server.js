89a47cdbc4d29a60803adbfa873e99392a3b72d4
BuildTools <unconfigured@null.spigotmc.org>
2018-06-02 08:25:49 +0200

    Added PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
@@ -50,13 +51,39 @@ app.get('/todos/:id', (req, res) => {
 });
 
 app.delete('/todos/:id', (req, res) => {
-    var id = req.params.id
+    var id = req.params.id;
 
     if (!ObjectID.isValid(id)) {
         return res.status(404).send('Id not valid');
     }
 
     Todo.findByIdAndRemove(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send('Todo not found');
+        }
+
+        res.send({todo});
+    }).catch((err) => {
+        res.send(400).send(err);
+    });
+});
+
+app.patch('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    var body = _.pick(req.body, ['text', 'completed']);
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send('Id not valid');
+    }
+
+    if (_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
         if (!todo) {
             return res.status(404).send('Todo not found');
         }
